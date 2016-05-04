dfe7492378e2a02fe9528382bbd42103ec2e8794
Angad Gill <angadsinghgill@gmail.com>
2016-05-03 22:03:45 -0700

    Added local DynamoDB support

--- README.md
+++ README.md
@@ -9,12 +9,25 @@ python -m SimpleHTTPServer 8000
 Open a web browser and navigate to `localhost:8000/boilerplate-html/index.html`
 Use the console to see that PyWebAnalytics has loaded with no errors.  
 
-##### To run the `collector` server:
+##### How to run the `collector` server:
+Install a local copy of DynamoDB. See guide [here](http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Tools.DynamoDBLocal.html).
+Set environment variables as follows:
+
+```
+export DYNAMO_ENABLE_LOCAL=True  
+export DYNAMO_LOCAL_HOST='localhost'  
+export DYNAMO_LOCAL_PORT=8080  
+```
+
+Run the DynamoDB local instance:
+```
+java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -port 8080
+```
+
+To run the `collector server`, run commands as follows:  
 ```
 cd python-web-analytics/collector
 python collector.py
 ```
 This server accepts GET requests at this address: `localhost:5000/collect` and prints
-data received as URL params to the terminal.
-
-
+data received as URL params to the terminal.  
--- collector/collector.py
+++ collector/collector.py
@@ -6,13 +6,47 @@ Author: Angad Gill
 """
 
 from flask import Flask, request
+from flask.ext.dynamo import Dynamo
+
+from boto.dynamodb2.fields import HashKey
+from boto.dynamodb2.table import Table
+
+import time
+
+
 app = Flask(__name__)
+app.config['DYNAMO_ENABLE_LOCAL'] = True
+app.config['DYNAMO_LOCAL_HOST'] = 'localhost'
+app.config['DYNAMO_LOCAL_PORT'] = 8000
+
+
+app.config['DYNAMO_TABLES'] = [
+    Table('pages', schema=[HashKey('key')]),
+]
+
+
+dynamo = Dynamo(app)
+
+# with app.app_context():
+#     dynamo.create_all()
+#     for table_name, table in dynamo.tables.iteritems():
+#         print table_name, table
 
 
 @app.route('/collect', methods=['GET'])
 def index():
-    print request.args
-    return "Collected!"
+    pagename = request.args.get('pagename')
+    time_stamp = time.time()
+    user_id = 99999
+    key = pagename + '_' + str(user_id) + '_' + str(time_stamp)
+    print pagename
+    dynamo.tables['pages'].put_item(data={
+        'key': key,
+        'pagename': pagename,
+        'time': time_stamp,
+        'user_id': user_id
+    })
+    return ""  # Super small response sent to client
 
 
 if __name__ == '__main__':
