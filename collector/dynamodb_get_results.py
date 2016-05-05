9ba0911591243b8237403e4bf823dcbca336c640
Angad Gill <angadsinghgill@gmail.com>
2016-05-04 22:19:49 -0700

    Added script to get results from DB

---
+++ collector/dynamodb_get_results.py
@@ -0,0 +1,20 @@
+"""
+Use this script to print results from DynamoDB in terminal.
+
+Author: Angad Gill
+"""
+
+from flask import Flask
+from flask.ext.dynamo import Dynamo
+import dynamodb_config
+
+app = Flask(__name__)
+print dynamodb_config.DYNAMO_TABLES
+app.config['DYNAMO_TABLES'] = dynamodb_config.DYNAMO_TABLES
+dynamo = Dynamo(app)
+
+with app.app_context():
+    table = dynamo.tables['pages']
+    results = table.scan()
+    for result in results:
+        print "Pagename: %s, \t count: %d" % (result['pagename'], result['count'])
