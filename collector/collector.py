b9ab787855dabf5bbe43adca589d34f00a1a30b9
Angad Gill <angadsinghgill@gmail.com>
2016-05-04 22:29:32 -0700

    DB touched only if pagename provided

--- collector/collector.py
+++ collector/collector.py
@@ -18,32 +18,34 @@ dynamo = Dynamo(app)  # Assumes that DynamoDB tables are already setup
 
 @app.route('/collect', methods=['GET'])
 def index():
-    pagename = str(request.args.get('pagename'))
+    pagename = request.args.get('pagename')
     time_stamp = time.time()
     # TODO: Replace 99999 below with actual user_id
     user_id = 99999
-    print "Page visit at:", pagename
-    with app.app_context():
-        table = dynamo.tables['pages']
 
-        # Update existing pagename value
-        # Assumes that pagename is the key
-        if table.has_item(pagename=pagename):
-            print "Updating existing entry..."
-            item = table.get_item(pagename=pagename)
-            item['time'] += [time_stamp]
-            item['user_id'] += [user_id]
-            item['count'] += 1
-            table.put_item(item, overwrite=True)
-        else:
-            # If pagename doesn't exist, create a new one
-            print "Creating new entry for page..."
-            table.put_item(data={
-                'pagename': pagename,
-                'time': [time_stamp],
-                'user_id': [user_id],
-                'count': 1
-            })
+    if pagename:  # pagename is not None
+        print "Page visit at:", pagename
+        with app.app_context():
+            table = dynamo.tables['pages']
+
+            # Update existing pagename value
+            # Assumes that pagename is the key
+            if table.has_item(pagename=pagename):
+                print "Updating existing entry..."
+                item = table.get_item(pagename=pagename)
+                item['time'] += [time_stamp]
+                item['user_id'] += [user_id]
+                item['count'] += 1
+                table.put_item(item, overwrite=True)
+            else:
+                # If pagename doesn't exist, create a new one
+                print "Creating new entry for page..."
+                table.put_item(data={
+                    'pagename': pagename,
+                    'time': [time_stamp],
+                    'user_id': [user_id],
+                    'count': 1
+                })
     return ""  # Super small response sent to client
 
 
