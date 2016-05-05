e39478135fb8bd759eeba2cc4afa84aba4e63025
Brandon Huynh <yukibhuynh@gmail.com>
2016-05-05 00:12:58 -0700

    pwanalytics now sends tracking data to server via image request

--- pwanalytics.js
+++ pwanalytics.js
@@ -8,25 +8,36 @@ function PageviewTracker (name, pagename) {
 
 function PyWebAnalytics() {
 
-  this.send_to_server_ = function(data) {
-    //TODO: Send data to server. Where? Which server?
+  this.data = {};
+
+  // Serialize dictionary data into querystring
+  // From: http://stackoverflow.com/questions/1714786/querystring-encoding-of-a-javascript-object
+  this.serialize = function(obj) {
+    var str = [];
+    for(var p in obj)
+      if (obj.hasOwnProperty(p)) {
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      }
+    return str.join("&");
   }
-  this.send = function(tracker_type) {
-    if (tracker_type == "pageview") {
-      //TODO: Instantiate tracker object? 
-    }
+
+  this.send = function() {
+    var i = new Image();
+    var request_url = "//localhost:5000/collect?" + this.serialize(this.data);
+    i.src = request_url;
+  }
+
+  this.set_param = function(param, value) {
+    this.data[param] = value; 
   }
 }
 
-// Serialize dictionary data into querystring
-// From: http://stackoverflow.com/questions/1714786/querystring-encoding-of-a-javascript-object
-serialize = function(obj) {
-  var str = [];
-  for(var p in obj)
-    if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-    }
-  return str.join("&");
-}
+var pwa = new PyWebAnalytics();
+
+// Set pagename to be URL for now
+pwa.set_param("pagename", window.location.href);
+
+// Phone home (to collector)
+pwa.send();
 
 console.log("PyWebAnalytics has loaded!");
